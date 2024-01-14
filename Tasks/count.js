2af7415f38fe8157375ab9cfd90a694cc32ec243
Самойленко Марія ІМ-32 <143123711+Pshonkey@users.noreply.github.com>
2024-01-14 22:37:59 +0200

    Update count.js

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,13 +1,13 @@
 // Sum all number values in dict
 'using strict';
-const count = (obj) => {
-  let sum = 0;
+const sumAllNumbers = (dict) => {
+  let sumOfNumbers = 0;
   const keys = Object.keys(obj);
   keys.forEach((key) => {
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  return sum;
+  return sumOfNumbers;
 };
 
-module.exports = count;
+module.exports = sumAllNumbers;
