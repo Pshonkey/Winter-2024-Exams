da93acfd1bd0626344fb4e9491e662857a345a7a
Самойленко Марія ІМ-32 <143123711+Pshonkey@users.noreply.github.com>
2024-01-14 23:57:55 +0200

    Update month.js

--- Tasks/month.js
+++ Tasks/month.js
@@ -2,8 +2,7 @@
 'use strict';
 const Months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 const Month = (s) => {
-  let l = Months.length;
-  for (let i = 0; i < l; i++) {
+  for (let i = 0; i < Months.length; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
   }
   return -1;
