b3b05e3ffe66c486e7a78807989c9e35a045bdd5
Самойленко Марія ІМ-32 <143123711+Pshonkey@users.noreply.github.com>
2024-01-15 00:03:36 +0200

    Update month.js

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,11 +1,11 @@
 // Get month number
 'use strict';
-const Months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
-const Month = (s) => {
-  for (let i = 0; i < Months.length; i++) {
-    if (s.toLowerCase().startsWith(Months[i])) return i + 1;
+const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+const getMonthNum = (input) => {
+  for (let i = 0; i < months.length; i++) {
+    if (input.toLowerCase().startsWith(months[i])) return i + 1;
   }
   return -1;
 };
 
-module.exports = Month;
+module.exports = getMonthNum;
