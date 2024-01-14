411adca22b83915fd7559b8524c8346271572a0f
Самойленко Марія ІМ-32 <143123711+Pshonkey@users.noreply.github.com>
2024-01-14 21:57:39 +0200

    Update day.js

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,14 +1,14 @@
 // Get day number
 
 'using strict';
-const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-const _parse_day_ = (s) => {
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const parseDay = (inputString) => {
   for (let i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+    if (inputString.startsWith(daysOfWeek[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
