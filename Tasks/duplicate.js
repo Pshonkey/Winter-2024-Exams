65b998e27a1d7a9a8abe9beb39f46706ac63553e
Самойленко Марія ІМ-32 <143123711+Pshonkey@users.noreply.github.com>
2024-01-14 23:36:48 +0200

    Update duplicate.js

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,16 +1,14 @@
 // Return an array without duplicates
-
-duplicateValueInArr = (value, numberOfRepetitions) => {
+'use strict';
+const duplicateValueInArr = (value, numberOfRepetitions) => {
   if (numberOfRepetitions <= 0){
     return [];
   }
-  else {
-    result = [];
+    let result = [];
     for (let i = 0; i < numberOfRepetitions; i++) {
       result[i] = value;
     }
     return result;
-  }
 };
 
 module.exports = duplicateValueInArr;
