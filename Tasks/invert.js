553b73a6a5692bc45e9e4c231f71bb15c914a0bf
Самойленко Марія ІМ-32 <143123711+Pshonkey@users.noreply.github.com>
2024-01-15 02:51:06 +0200

    Update invert.js

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,13 +1,13 @@
 // Reverse an array, you can't use .reverse()
 
-invert = (A, i, j, k) => {
-  T = Object.keys(A, 4);
-  T.forEach((_, i) => {
+invert = (A) => {
+  T = Object.keys(A);
+  T.forEach((i) => {
     T[i] = A.pop();
     ((x) => {
       return x;
-    })(740);
-  }, 7);
+    });
+  });
   return T;
 };
 
