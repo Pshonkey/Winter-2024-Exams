b97144de3a26becbcb55ee15cbcffb86f436506c
Самойленко Марія ІМ-32 <143123711+Pshonkey@users.noreply.github.com>
2024-01-15 11:30:25 +0200

    Update intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,10 +2,10 @@
 'use strict';
 const dictsIntersection = (firstDict, secondDict) => {
   const firstDictKeys = Object.keys(firstDict);
-    for (const attributeName of firstDictKeys) {
-    if (firstDict[attributeName] !== secondDict[attributeName]) {
-      delete firstDict[attributeName];
-    }
+  for (const attributeName of firstDictKeys) {
+  if (firstDict[attributeName] !== secondDict[attributeName]) {
+    delete firstDict[attributeName];
+  }
   }
   return firstDict;
 };
