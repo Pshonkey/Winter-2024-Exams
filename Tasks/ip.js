f45856796e45239ff8f37f46734849c60116562c
Самойленко Марія ІМ-32 <143123711+Pshonkey@users.noreply.github.com>
2024-01-15 03:21:56 +0200

    Update ip.js

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -2,16 +2,21 @@
 'use strict';
 const parseIP = (input) => {
   const result = [];
-  if (input === '') return;
-    const splittedArray = input.split('.');
-    if (splittedArray.length != 4) return;
-    let j = 0;
-    for (const key of splittedArray) {
-      result[j] = parseInt(key);
-      if (isNaN(result[j])) return;
-      j++;
-    }
-  return result;
+  if (input === '') { 
+    return;
+  }
+  const ipLength = 4;
+  const splittedArray = input.split('.');
+  if (splittedArray.length != ipLength) { 
+    return;
+  }
+  let j = 0;
+  for (const key of splittedArray) {
+    result[j] = parseInt(key);
+    if (isNaN(result[j])) return;
+    j++;
+  }
+return result;
 };
 
 module.exports = parseIP;
